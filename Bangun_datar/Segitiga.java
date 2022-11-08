de9862337cf9440ffdff36470adcb88e5a7488b1
Muhammad Fadilah Nurjaman <96771710+dhilz202@users.noreply.github.com>
2022-11-08 10:07:11 +0700

    Add files via upload

---
+++ Bangun_datar/Segitiga.java
@@ -0,0 +1,17 @@
+package Bangun_datar;
+
+public class Segitiga extends BangunDatar{
+
+    private float alas;
+    private float tinggi;
+
+    public Segitiga(float alas, float tinggi){
+        this.alas=alas;
+        this.tinggi=tinggi;
+    }
+
+    @Override
+    float getLuas(){
+        return (float)0.5*alas*tinggi;
+    }
+}
